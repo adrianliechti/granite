f7802ea90683faf822737a254b266f6c5eddf35b
Adrian Liechti <adrian@monobox.ch>
2026-01-03 14:30:43 +0100

    flexible port

--- cmd/granite/main.go
+++ cmd/granite/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"net"
+	"os/exec"
+	"runtime"
 
 	"github.com/adrianliechti/granite/pkg/config"
 	"github.com/adrianliechti/granite/pkg/server"
@@ -14,13 +19,65 @@ func main() {
 		panic(err)
 	}
 
+	port, err := getFreePort("localhost", 7777)
+
+	if err != nil {
+		panic(err)
+	}
+
 	srv, err := server.New(cfg)
 
 	if err != nil {
 		panic(err)
 	}
 
-	if err := srv.ListenAndServe(context.Background(), "localhost:7777"); err != nil {
+	url := fmt.Sprintf("http://localhost:%d", port)
+	addr := fmt.Sprintf("localhost:%d", port)
+
+	openBrowser(url)
+	fmt.Printf("Bridge is running at %s\n", url)
+
+	if err := srv.ListenAndServe(context.Background(), addr); err != nil {
 		panic(err)
 	}
+}
+
+func getFreePort(host string, port int) (int, error) {
+	if port > 0 {
+		listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", host, port))
+
+		if err == nil {
+			listener.Close()
+			return port, nil
+		}
+	}
+
+	listener, err := net.Listen("tcp", ":0")
+
+	if err != nil {
+		return 0, fmt.Errorf("failed to find a free port: %w", err)
+	}
+
+	defer listener.Close()
+
+	addr := listener.Addr().(*net.TCPAddr)
+	return addr.Port, nil
+}
+
+func openBrowser(url string) error {
+	switch runtime.GOOS {
+	case "darwin":
+		cmd := exec.Command("open", url)
+		return cmd.Start()
+
+	case "linux":
+		cmd := exec.Command("xdg-open", url)
+		return cmd.Start()
+
+	case "windows":
+		cmd := exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
+		return cmd.Start()
+	}
+
+	return errors.ErrUnsupported
 }
